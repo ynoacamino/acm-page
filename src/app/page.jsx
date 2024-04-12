8ef4f8542813ccda23f9913f2954a383a3f1057b
alexisBltz <almamanima@unsa.edu.pe>
2024-04-12 08:13:17 -0500

    Agregando tailwind

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      acm page
+    <main className=" flex flex-col gap-8">
     </main>
   );
 }
