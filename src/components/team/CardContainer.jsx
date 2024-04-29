14c58c2dccd008bc0884806f94fdc1c8387d694c
Joel <jchinop@unsa.edu.pe>
2024-04-29 15:51:10 -0500

    pruebas

--- src/components/team/CardContainer.jsx
+++ src/components/team/CardContainer.jsx
@@ -5,7 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 export default function CardContainer() {
-    let style = "flex flex-row flex-wrap gap-14 items-center justify-center m-[2%]"
+    let style = "flex flex-row flex-wrap gap-10 items-center justify-center m-[2%]"
 
     return (
         <div className={style}>
